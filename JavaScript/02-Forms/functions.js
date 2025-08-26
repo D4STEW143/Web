e90822afca2f86f81a61e47619d0860eb8fbc764
D4STEW143 <dastew143@mail.ru>
2025-08-26 23:14:02 +0900

    Homework partly done

--- JavaScript/02-Forms/functions.js
+++ JavaScript/02-Forms/functions.js
@@ -15,9 +15,22 @@ document.addEventListener("mousemove", GetMouseCoords);
 function SwitchBackground() {
 	let switchBackground = document.getElementById('switch-background');
 	if (switchBackground.style.backgroundImage === 'url("img/moon.png")') {
+		document.body.className = 'light-theme';
 		switchBackground.style.backgroundImage = 'url("img/sun.png")';
 	}
 	else {
+		document.body.className = 'dark-theme';
 		switchBackground.style.backgroundImage = 'url("img/moon.png")';
 	}
 }
+document.addEventListener("DOMContentLoaded", SwitchBackground);
+
+function ThemeChangeDelay() {
+	let slider = document.getElementById('theme - change - delay - slider');
+	let switchBackground = document.getElementById('switch-background');
+
+	let body = document.querySelector('body');
+	body.style.cssText += `transition: background-color ${slider.value}s ease`;
+}
+document.addEventListener("mouseup", ThemeChangeDelay);
+
