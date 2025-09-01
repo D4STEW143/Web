ecb930bd2864d3ef627d8f91be99ba4d0c537ed0
D4STEW143 <dastew143@mail.ru>
2025-09-01 10:41:40 +0900

    Branches creation

--- JavaScript/02-Forms/functions.js
+++ JavaScript/02-Forms/functions.js
@@ -1,45 +1,29 @@
 // JavaScript source code
 function Power() {
-	let base = document.getElementById("base").value;
-	let exponent = document.getElementById("exponent").value;
-	document.getElementById("power").innerHTML = base ** exponent;
-}
-
-function GetMouseCoords(event) {
-	let x = event.clientX;
-	let y = event.clientY;
-	document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y};`
+    let base = document.getElementById("base").value;
+    let exponent = document.getElementById("exponent").value;
+    document.getElementById("power").innerHTML = base ** exponent;
 }
 document.addEventListener("mousemove", GetMouseCoords);
-
+function GetMouseCoords(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
+}
 function SwitchBackground() {
-	let switchBackground = document.getElementById('switch-background');
-	if (switchBackground.style.backgroundImage === 'url("img/moon.png")') {
-		document.body.className = 'light-theme';
-		switchBackground.style.backgroundImage = 'url("img/sun.png")';
-	}
-	else {
-		document.body.className = 'dark-theme';
-		switchBackground.style.backgroundImage = 'url("img/moon.png")';
-	}
+    let switchBackground = document.getElementById("switch-background");
+    //if(switchBackground.style.backgroundImage === 'url("img/moon.png")')
+    //{
+    //    switchBackground.style.backgroundImage = 'url("img/sun.png")';
+    //    document.body.className = 'dark';
+    //}
+    //else
+    //{
+    //    switchBackground.style.backgroundImage = 'url("img/moon.png")';
+    //    document.body.className = 'light';
+    //}
+    let delay = document.getElementById('delay').value;
+    document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
+    document.getElementById('switch-background').style.transition = `background-image ${delay}s`;
+    document.body.className = document.body.className === "light" ? "dark" : "light";
 }
-document.addEventListener("DOMContentLoaded", SwitchBackground);
-
-function ThemeChangeDelay() {
-	let slider = document.getElementById('theme - change - delay - slider');
-	let switchBackground = document.getElementById('switch-background');
-
-	
-
-	//let body = document.querySelector('body');
-	//body.style.cssText += `transition: background-color ${slider.value}s ease`;
-}
-document.addEventListener("mouseup", ThemeChangeDelay);
-
-let slider = document.getElementById('theme-change-delay-slider');
-	slider.addEventListener('input', function () {
-		document.documentElement.style.setProperty('--theme-transition-delay', `${this.value}s`);
-	});
-
-
-
